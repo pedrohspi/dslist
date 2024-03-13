cd7f723cca150ebdf30ec520aa7a31ba4ac198e7
Pedro <pedro.pereira.@ccc.ufcg.edu.br>
2024-03-12 22:23:28 -0300

    properties,Game,ORM

---
+++ src/main/java/com/apirest/dslist/models/Game.java
@@ -0,0 +1,24 @@
+package com.apirest.dslist.models;
+
+import jakarta.persistence.*;
+
+@Entity(name = "game")
+public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    @Column(name = "game_year")
+    private Integer year;
+    private String genre;
+    private String platform;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+
+    public Game () {
+    }
+
+
+
+}
